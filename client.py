cd0cd4255f856d50c9fe97f6eaa49937b0079e31
FermiD <55369111+FermiD@users.noreply.github.com>
2021-06-29 16:26:17 +0700

    Tidying the comments and confirmations

--- client.py
+++ client.py
@@ -1,28 +1,28 @@
 import socket
 
 HEADER = 64
-PORT = 3128
+PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.56.1"#my computer as server
+SERVER = "192.168.56.1" #computer as the server
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket setup
-client.connect(ADDR)#connecting client n server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket setup
+client.connect(ADDR) #connecting client and the server
 
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)#encodeinutf8
-    send_length += b' ' * (HEADER - len(send_length))#to make the msg the correct length. we use 64bytes
+    send_length = str(msg_length).encode(FORMAT) #encode in utf8
+    send_length += b' ' * (HEADER - len(send_length)) #to make the msg the correct length. we use 64bytes
     client.send(send_length)
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
 
 send("Hello World!")
 input()
-send("Hello World Second Message!")
+send("Hello World, Second Message!")
 input()
-send("Hello World Third Message!")
+send("Hello World, Third Message!")
 
 send(DISCONNECT_MESSAGE)
